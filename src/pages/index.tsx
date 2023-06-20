5b6d11e9c9a90872b792c69a38f6797806f4cfa0
adrian.andrei <adrian.andrei@wisesys.ro>
2023-06-20 22:49:32 +0300

    working posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,21 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+  const [input, setInput] = useState("");
+  const ctx = api.useContext();
+
+  const {mutate, isLoading: isPosting} = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    }
+  });
   if (!user) return null;
 
   return (
@@ -25,7 +35,11 @@ const CreatePostWizard = () => {
         type="text"
         placeholder="type something..."
         className="grow bg-transparent outline-none"
+        value={input}
+        disabled={isPosting}
+        onChange={(e) => setInput(e.target.value)}
       />
+      <button onClick={() => mutate({content: input}) }>Post</button>
     </div>
   );
 };
@@ -63,7 +77,7 @@ const Feed = () => {
   if (!data) return <div>Something went wrong</div>;
   return (
     <div>
-      {[...data, ...data]?.map((fullPost) => (
+      {data.map((fullPost) => (
         <PostView {...fullPost} key={fullPost.post.id} />
       ))}
     </div>
